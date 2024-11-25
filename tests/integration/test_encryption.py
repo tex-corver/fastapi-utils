b3869e3587981424c3a1015da608107591613899
Kwang1402 <hongquang2k4@gmail.com>
2024-11-26 01:29:10 +0700

    NEXT-145: refactor tests

--- tests/integration/test_encryption.py
+++ tests/integration/test_encryption.py
@@ -1,7 +1,11 @@
 import pytest
 from icecream import ic
 
+import utils
 from fastapi_utils.dependency import authorize
+
+
+config = utils.get_config()
 
 
 class TestGetDecryptionKey:
@@ -9,7 +13,14 @@ class TestGetDecryptionKey:
         key = authorize.get_decryption_key()
         ic(key)
 
+
 class TestDownloadDecryptionKey:
     def test_download_decryption_key(self):
         key = authorize.download_decryption_key()
         ic(key)
+        assert (
+            key
+            == open(
+                config["application"]["encryption"]["jwt"]["public_key"], "rb"
+            ).read()
+        )
