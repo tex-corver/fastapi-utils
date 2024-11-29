87c88bdb0d35caedd7e0442b99cae1bcb025256a
Luna <luna@master>
2024-11-29 21:19:02 +0000

    add pre-commit and ci test

--- tests/integration/test_encryption.py
+++ tests/integration/test_encryption.py
@@ -1,15 +1,15 @@
-import pytest
-from icecream import ic
+from icecream import ic  # type: ignore
 
-from fastapi_utils.dependency import authorize
+from fastapi_utils.dependency import authorize  # type: ignore
 
 
 class TestGetDecryptionKey:
-    def test_get_decryption_key(self):
+    def test_get_decryption_key(self) -> None:
         key = authorize.get_decryption_key()
         ic(key)
 
-class TestDownloadDecryptionKey:
-    def test_download_decryption_key(self):
-        key = authorize.download_decryption_key()
-        ic(key)
+
+# class TestDownloadDecryptionKey:
+#     def test_download_decryption_key(self):
+#         key = authorize.download_decryption_key()
+#         ic(key)
